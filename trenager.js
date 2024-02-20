e58553462a35d76ba34a3cf15389f40b1dd0d970
Georg196 <oooppprrr@gmail.com>
2024-02-20 22:42:40 +0500

    временный файл

---
+++ trenager.js
@@ -0,0 +1,10 @@
+let num = 124
+let str = 'Kykylaco'
+let strNum = '111'
+let strNot = ''
+let bul = true
+let bulNot = false
+
+res = num < strNum ||  bul && bulNot && 1 && !null || null
+
+console.log(res ?? 'Error')
